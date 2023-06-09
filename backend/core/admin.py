84262a2c1d5b98c60165d3e8cebf42414261c56e
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2023-06-09 14:14:12 +0800

    fix: add manager to User, change date_joined to auto_now_add

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from backend.core.models import User
+from .models import User
 
 
 @admin.register(User)
--- backend/core/models.py
+++ backend/core/models.py
@@ -32,7 +32,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(auto_created=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = UserManager()
